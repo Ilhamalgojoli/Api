c73d35a08d46254bb5af67a3b261300f81459f80
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-08-29 11:00:20 +0700

    Change logic on sign_in

--- service/service.js
+++ service/service.js
@@ -1,3 +1,4 @@
+const crypt = require('bcrypt');
 const database = require('../util/server');
 
 const sign_up = async (data) => {
@@ -12,36 +13,40 @@ const sign_up = async (data) => {
         };
     }
 
-    const query = await database.query(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, password]);
-    return database.query(
-        query,
-        [username, password],
-        {
+    const hashPassword = await crypt.hash(password, 10);
+
+    const query = await database.query(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, hashPassword]);
+    return ({
             success: true,
             message: "User created successfully"
-        });
+    });
 };
 
 const sign_in = async (data) => {
     const { username, password } = data;
 
-    const [exists] = await database.query(`SELECT FROM users WHERE password = ?`, [password]);
+    const [exists] = await database.query(`SELECT FROM users WHERE password = ?`, [username]);
 
-    if (password !== exists[0].password) {
+    if (exists.legth === 0) {
         return {
             success: false,
-            message: "Password is incorrect"
+            message: "Password or username is incorrect"
         };
     }
 
-    const query = await database.query(`SELECT * FROM users WHERE username = ? AND password = ?`);
-    return database.query(
-        query,
-        [username, password],
-        {
+    const user = rows[0];
+    const isMatch = await crypt.compare(password, user.password);
+    
+    if (!isMatch) {
+        return {
+            success: false,
+            message: "Password or username is incorrect"
+        };
+    }
+    return ({
             success : true,
             message : "User signed in successfully" 
-        });
+    });
 };
 
 module.exports = {
