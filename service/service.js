b5a11a7a82691483afa2133ad849dbb0943f4d29
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-08-29 10:45:26 +0700

    Create sign_in service

--- service/service.js
+++ service/service.js
@@ -1,13 +1,50 @@
 const database = require('../util/server');
 
-const sign_up = async((data) => {
-    const { username, password } = data ;
-    const query = `INSERT INTO users (username, password) VALUES (?, ?)`;
-    return database.query(query, [username, password]); 
-});
+const sign_up = async (data) => {
+    const { username, password } = data;
 
-const sign_in = async((data) => {
-    const { username, password } = data ;
-    const query = `SELECT * FROM users WHERE username = ? AND password = ?`;
-    return database.query(query, [username, password]);
-});
+    const [exists] = await database.query(`SELECT FROM users WHERE username = ?`, [username]);
+
+    if (exists.legth > 0) {
+        return {
+            success: false,
+            message: "Username is already exists"
+        };
+    }
+
+    const query = await database.query(`INSERT INTO users (username, password) VALUES (?, ?)`, [username, password]);
+    return database.query(
+        query,
+        [username, password],
+        {
+            success: true,
+            message: "User created successfully"
+        });
+};
+
+const sign_in = async (data) => {
+    const { username, password } = data;
+
+    const [exists] = await database.query(`SELECT FROM users WHERE password = ?`, [password]);
+
+    if (password !== exists[0].password) {
+        return {
+            success: false,
+            message: "Password is incorrect"
+        };
+    }
+
+    const query = await database.query(`SELECT * FROM users WHERE username = ? AND password = ?`);
+    return database.query(
+        query,
+        [username, password],
+        {
+            success : true,
+            message : "User signed in successfully" 
+        });
+};
+
+module.exports = {
+    sign_up,
+    sign_in
+}
