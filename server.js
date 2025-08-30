79e31998ef9fea0d5d02bd091b399a336e3f3862
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-08-30 16:35:23 +0700

    Change database to postrage and solve any problem on code

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+const router = require('./routes/route');
+
+app.use(express.json());
+app.use("/auth", router);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
