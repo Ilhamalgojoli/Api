256d49ef5903b9c5d4840faa97b5bdbc100f5bf3
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-08-30 17:03:04 +0700

    success solve problem on connect databsae

--- util/database.js
+++ util/database.js
@@ -1,26 +1,27 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const { pool } = new Pool({
-    user: process.env["DB_USER "],
-    password: process.env["DB_PASSWORD "],
-    host: process.env["DB_HOST "],
-    port: process.env["DB_PORT "],
-    database: process.env["DB_NAME "],
-})
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
 
 async function checkConnection(){
     try{
         const client = await pool.connect();
-        if (!client){
-            console.log("Connection failed to database")
-        }
-
-        console.log("Success to connect database");
+        const resultTest = await client.query('SELECT * FROM users');
+        console.log("Success to connect database", resultTest);
         client.release();
+
+
     } catch (err){
         console.error("Error to connect database", err)
     }
 }
+
+checkConnection();
 
 module.exports = pool;
